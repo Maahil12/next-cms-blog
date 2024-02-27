b5e53a2a7f584730536a2c0a7412a6fed2430c06
Maahil <stormtownmaahil@gmail.com>
2024-02-28 04:12:06 +0500

    added all the files

--- app/components/CmsNavbar.tsx
+++ app/components/CmsNavbar.tsx
@@ -12,8 +12,8 @@ const CmsNavbar = () => {
       </Link>
 
       <div className={`${font.className} text-3xl dark:text-amber-50`}>
-        Dev
-        <span className="text-purple-500">Blook</span>
+        Man
+        <span className="text-purple-500">ta</span>
       </div>
     </div>
   );
